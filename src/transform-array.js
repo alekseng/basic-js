de3116fd342cc71fa1d2ac4613620224c5b5de6e
alekseng <alexvdche@mail.ru>
2023-12-19 23:04:51 +0300

    feat: complete transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  const tr = [
+    '--discard-prev',
+    '--double-prev',
+    '--double-next',
+    '--discard-next'
+  ];
+  if (Array.isArray(arr) === false
+    ||
+    Array.isArray(arr) === undefined
+    ||
+    Array.isArray(arr) === null) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const array = structuredClone(arr);
+  const res = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === tr[0]) {
+      if (array[i - 1]) {
+        array.splice(i - 1, 1);
+        i -= 1;
+      }
+    }
+    else if (array[i] === tr[1]) {
+      if (array[i - 1]) {
+        array[i] = array[i - 1];
+        i -= 1;
+      }
+    }
+    else if (array[i] === tr[2]) {
+      if (array[i + 2] === tr[0]) {
+      } else if (array[i + 1]) {
+        array[i] = array[i + 1];
+        i -= 1;
+      } else {
+        array.splice(i, 1);
+      }
+    }
+    else if (array[i] === tr[3]) {
+      if (array[i + 1]) {
+        array.splice(i + 1, 1);
+        i += 1;
+      } else {
+        array.splice(i, 1);
+      }
+    }
+    else {
+      res.push(array[i]);
+    }
+  }
+  return res;
 }
 
 module.exports = {
